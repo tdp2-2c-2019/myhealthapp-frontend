ea7d96587410b23aaa67d4159f4abd6c4fe0266f
Martín Stancanelli <mastanca14@gmail.com>
2019-11-24 17:55:57 -0300

    Format

--- src/views/Metrics/Metrics.jsx
+++ src/views/Metrics/Metrics.jsx
@@ -18,27 +18,20 @@ const optionsForPieChart = {
   tooltips: {
     enabled: true,
     callbacks: {
-      // this callback is used to create the tooltip label
-      label: function (tooltipItem, data) {
-        // get the data label and data value to display
-        // convert the data value to local string so it uses a comma seperated number
+      label: (tooltipItem, data) => {
         let dataLabel = ['Cantidad', 'Porcentaje'];
         const rawNumberValue = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
         const value = ': ' + rawNumberValue.toLocaleString();
         const totalValue = data.datasets[0].data.reduce((total, value) => { return total + parseInt(value) }, 0);        
 
-        // make this isn't a multi-line label (e.g. [["label 1 - line 1, "line 2, ], [etc...]])
         if (Chart.helpers.isArray(dataLabel)) {
-          // show value on first line of multiline label
-          // need to clone because we are changing the value
           dataLabel = dataLabel.slice();
           dataLabel[0] += value;
           dataLabel[1] += `: ${((rawNumberValue/totalValue).toFixed(2))}%`;
         } else {
           dataLabel += value;
         }
-
-        // return the text to display on the tooltip
+        
         return dataLabel;
       }
     }
